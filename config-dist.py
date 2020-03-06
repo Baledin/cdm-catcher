dfc7c441881e9135f6424aeb0bc2073d05cc0fc7
Stroud <jeremy.stroud@secstate.wa.pri>
2020-03-05 17:24:04 -0800

    Initial py script

---
+++ config-dist.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+cdm = {
+    'url': 'http://yourcollection.yourorgname.org:port',
+    'username': 'johndoe',
+    'password': 'password123',
+    'license': '12345-6789a-bcdef-ghijk',
+}
