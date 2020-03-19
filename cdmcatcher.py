db6c72db52522871354068d3a62da792de852def
Jeremy Stroud <jeremy@jiveconsulting.com>
2020-03-19 10:41:12 -0700

    Renamed methods using PEP8 standards. Initial method declarations for FileProcessor

--- cdmcatcher.py
+++ cdmcatcher.py
@@ -1,5 +1,8 @@
 import argparse
 import requests
+import json
+import xml.etree.ElementTree as xtree
+import xmltodict
 from zeep import Client
 from config import cdm
 import os
@@ -15,7 +18,7 @@ config = {
     'license': cdm['license'],
 }
 
-def getArgs():
+def get_args():
     parser = argparse.ArgumentParser(description="%(prog)s is a python helper script for interacting with the CONTENTdm Catcher SOAP service.")
     
     # General arguments
@@ -33,18 +36,18 @@ def getArgs():
     
     # Add parser
     process_add_parser = subparsers.add_parser("add", help="Add metadata.")
-    process_add_parser.add_argument("filepath", action=ValidateFile, help="XML or JSON filepath with metadata to add.")
     process_add_parser.add_argument("collection", help="The alias of the collection to modify.")
+    process_add_parser.add_argument("filepath", action=FileProcessor, help="XML or JSON filepath with metadata to add.")
 
     # Delete parser
     process_delete_parser = subparsers.add_parser("delete", help="Add metadata.")
-    process_delete_parser.add_argument("filepath", action=ValidateFile, help="XML or JSON filepath with metadata to delete.")
     process_delete_parser.add_argument("collection", help="The alias of the collection to modify.")
+    process_delete_parser.add_argument("filepath", action=FileProcessor, help="XML or JSON filepath with metadata to delete.")
     
     # Edit parser
     process_edit_parser = subparsers.add_parser("edit", help="Add metadata.")
-    process_edit_parser.add_argument("filepath", action=ValidateFile, help="XML or JSON filepath with metadata transformations to apply.")
     process_edit_parser.add_argument("collection", help="The alias of the collection to modify.")
+    process_edit_parser.add_argument("filepath", action=FileProcessor, help="XML or JSON filepath with metadata transformations to apply.")
 
     # Vocabulary parser
     vocab_parser = subparsers.add_parser("vocab", help="Return controlled vocabulary terms for selected collection.")
@@ -53,7 +56,7 @@ def getArgs():
     
     return parser.parse_args()
 
-def getParams(args):
+def get_params(args):
     valid_actions = [ 'add', 'delete', 'edit' ]
     params = { **config }
     for key, value in args.items():
@@ -77,7 +80,7 @@ def print_dictionary(**kwargs):
 
 def main(args):
     catcher = Client(CATCHERURL)
-    functionName = args.action
+    functionName = functions[args.action]
 
     if(args.version):
         print("Catcher version: " + catcher.service.getWSVersion())
@@ -87,7 +90,7 @@ def main(args):
     #result = eval("catcher.service." + functionName + "(" + functions[functionName]() + ")")
     #output(result, filename=functionName + ".xml")
     print(args)
-    print_dictionary(**getParams(vars(args)))
+    print_dictionary(**get_params(vars(args)))
 
 # Matches program argument with Catcher function:
 functions = {
@@ -99,14 +102,38 @@ functions = {
     'edit'      : 'processCONTENTdm'
 }
 
-class ValidateFile(argparse.Action):
-    EXT = ('.json', '.xml')
+class FileProcessor(argparse.Action):
+    ALLOWABLE_EXTENSIONS = ('json', 'xml')
+    filepath = ''
+    extension = ''
+    file_contents = {}
 
-    def __call__(self, parser, namespace, filename, option_string=None):
-        if not os.path.exists(filename):
-            parser.error("File does not exist.")
-        if not filename.endswith(self.EXT):
+    def __call__(self, parser, namespace, filepath, option_string=None):
+        self.filepath = filepath
+        self.extension = filepath[filepath.rindex(".")+1:]
+
+        if not self.extension in self.ALLOWABLE_EXTENSIONS:
             parser.error("Filename must have a .xml or .json extension.")
-        setattr(namespace, self.dest, filename)
+        if not os.path.exists(filepath):
+            parser.error("File does not exist.")
+        
+        setattr(namespace, self.dest, self.filepath)
+    
+    def get_file_contents(self):
+        return self.file_contents
 
-main(getArgs())
+    def set_file_contents(self):
+        parser = "parse_" + self.extension
+        self.file_contents = getattr(self, parser)
+
+
+    def parse_json(self):
+        with open(self.filepath) as f:
+            return json.load(f)
+    
+    def parse_xml(self):
+        with open(self.filepath) as f:
+            return f.test
+
+
+main(get_args())
